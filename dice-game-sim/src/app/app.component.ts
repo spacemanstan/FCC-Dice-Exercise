c75ef0e23779609c98381ef5e3cc550b7d70128a
spacemanstan <ariston.bodnarchuk@gmail.com>
2024-02-16 10:39:48 -0600

    removed place holder, initialized dice game sim component

--- dice-game-sim/src/app/app.component.ts
+++ dice-game-sim/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { DiceGameSimComponent } from './dice-game-sim/dice-game-sim.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, DiceGameSimComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ dice-game-sim/src/app/dice-game-sim/dice-game-sim.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dice-game-sim',
+  standalone: true,
+  imports: [],
+  templateUrl: './dice-game-sim.component.html',
+  styleUrl: './dice-game-sim.component.scss'
+})
+export class DiceGameSimComponent {
+
+}
